4bc3cdb08b2002fb8318fbf8499283116f6bad0b
Federico Verly <federicoverly@gmail.com>
2022-09-26 14:44:28 +0200

    channel_card

--- src/components/ChannelDetail.tsx
+++ src/components/ChannelDetail.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { Box, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { IChannel, IVideo } from "../interfaces";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { ChannelCard, Videos } from "./index";
 
 const ChannelDetail = () => {
-  return <div>ChannelDetail</div>;
+  const [channelDetail, setChannelDetail] = useState<IChannel>();
+  const [videos, setVideos] = useState<IVideo[]>([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
+
+      setChannelDetail(data?.items[0]);
+
+      const videosData = await fetchFromAPI(
+        `search?channelId=${id}&part=snippet&order=date`
+      );
+
+      setVideos(videosData?.items);
+    };
+
+    fetchResults();
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+        <ChannelCard channel={channelDetail} />
+      </Box>
+    </Box>
+  );
 };
 
 export default ChannelDetail;
--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import { Stack } from "@mui/material";
+import { Box } from "@mui/material";
 import { IVideo } from "../interfaces";
+import { ChannelCard, VideoCard } from "./index";
 
 interface IProps {
   videos: IVideo[];
 }
 
 const Videos = ({ videos }: IProps) => {
-  return <div>Videos</div>;
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((video, idx) => (
+        <Box key={idx}>
+          {video.id.videoId && <VideoCard video={video} />}
+          {video.id.channelId && <ChannelCard channelVideo={video} />}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
