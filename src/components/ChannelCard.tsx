e904e2213753ff811f4cb8ddc666fb8481bdfbd1
Federico Verly <federicoverly@gmail.com>
2022-09-28 21:28:43 +0200

    channel_card

--- src/components/ChannelCard.tsx
+++ src/components/ChannelCard.tsx
@@ -55,16 +55,6 @@ const ChannelCard = ({ channel, channelVideo }: IProps) => {
             {channel ? channel?.snippet?.title : channelVideo?.snippet.title}{" "}
             <CheckCircle sx={{ fontSize: 14, color: "gray", ml: "px" }} />
           </Typography>
-          {channel?.statistics?.subscriberCount && (
-            <Typography
-              sx={{ fontSize: "15px", fontWeight: 500, color: "gray" }}
-            >
-              {parseInt(channel?.statistics?.subscriberCount).toLocaleString(
-                "en-US"
-              )}{" "}
-              Subscribers
-            </Typography>
-          )}
         </CardContent>
       </Link>
     </Box>
